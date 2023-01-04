253dd11a1ca6f7f6cb6979be5b721c0771adcefa
yuseonpark <pyuseon@gmail.com>
2023-01-04 20:28:24 +0900

    etc : effective java item 29,30

---
+++ src/item30/Max.java
@@ -0,0 +1,16 @@
+package item30;
+
+import java.util.Collection;
+import java.util.Objects;
+
+public class Max {
+    public static <E extends Comparable<E>> E max(Collection<E> c) {
+        if (c.isEmpty())
+            throw new IllegalArgumentException("컬렉션이 비어 있습니다.");
+        E result = null;
+        for (E e : c)
+            if (result == null || e.compareTo(result) > 0)
+                result = Objects.requireNonNull(e);
+        return result;
+    }
+}
